3ca02e65e45d5bcd0343cb4d8c3aa6659b750b55
vishalpand <vishal.pandey0070@gmail.com>
2025-02-10 23:59:00 +0530

    ionic config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,17 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'asap.app',
+  appId: 'com.asap.app',
   appName: 'asap',
   webDir: 'www',
+  plugins: {
+    LiveUpdates: {
+      appId: '042a1261',
+      channel: 'Production',
+      autoUpdateMethod: 'background',
+      maxVersions: 2,
+    },
+  },
 };
 
 export default config;
