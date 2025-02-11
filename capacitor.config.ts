68e279b23bf634fd623311bc4d3297bf68eab08f
vishalpand <vishal.pandey0070@gmail.com>
2025-02-12 00:28:13 +0530

    capisitore

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,14 @@ const config: CapacitorConfig = {
   appId: 'asap.app',
   appName: 'asap',
   webDir: 'www',
+  plugins: {
+    LiveUpdates: {
+      appId: '6b1f76a6',
+      channel: 'Production',
+      autoUpdateMethod: 'background',
+      maxVersions: 2,
+    },
+  },
 };
 
 export default config;
